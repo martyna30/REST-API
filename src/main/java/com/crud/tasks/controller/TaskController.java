b35873285f59034e7412a5f72f837fabe0439fcb
martyna30 <prawdzikmartyna@op.pl>
2020-04-09 13:54:15 +0200

    TaskController

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -4,26 +4,20 @@ import com.crud.tasks.domain.TaskDto;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/task")//generowanie adresu dla api pod którym dany Controller będzie przyjmował żądania
+@RequestMapping("/v1/task")
 public class TaskController {
-    @RequestMapping(method = RequestMethod.GET, value = "getTasks")//nadanie ścieżek dla metod Controllera,
-    // aby Controller wiedział, z której z metod chcemy skorzystać,(method) -poinformowanie Springa,
-    // jakiego rodzaju żądania ma oczekiwać pod podanym adresem, aby wszystkie inne żądania były odrzucane.
-    // Kolejnym parametrem jest value i jest to nazwa mapowania, czyli to, co znajdzie się w adresie URL.
-    //http://localhost:8080/v1/task/getTasks  -URL do pobrania żądań
-
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
         return new ArrayList<>();
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L,"","");
+        return new TaskDto(1L,"test title","test_content");
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
@@ -33,7 +27,7 @@ public class TaskController {
 
     @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
     public TaskDto updateTask(TaskDto taskDto) {
-        return new TaskDto(1L,"","");
+        return new TaskDto(1L,"Edited test title","Test content");
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createTask")
